a4cd85fc8159aca01e366066987e2b9d4a74dab9
Matthew Hagerty <matthew@codehackcreate.com>
2017-01-28 12:09:41 -0800

    Comments and cleanup.

--- decode.vhdl
+++ decode.vhdl
@@ -21,9 +21,9 @@ entity decode is
    port (
       clk_i          : in  std_logic;
       reset_n_i      : in  std_logic;                    -- active low
-      
+
       ir_r_i         : in  std_logic_vector(0 to 15);    -- Instruction Register
-      
+
       byte_s_o       : out std_logic;                    -- byte selector
       Td_s_o         : out std_logic_vector(0 to 1);     -- destination mode: Td
       Dc_s_o         : out std_logic_vector(0 to 3);     -- destination: D or C
@@ -42,15 +42,15 @@ architecture rtl of decode is
    -- TODO convert to input generic, default to '00000'
    constant MEMCYCLE_COUNT          : std_logic_vector(0 to 4) := "00001";
 
-   
+   -- Microcode constants
    constant Y                       : std_logic := '1';
    constant N                       : std_logic := '0';
    constant MCODE_JMP               : std_logic := '1';
    constant MCODE_RET               : std_logic := '1';
    constant NEXT_OP                 : std_logic_vector(0 to 9)  := "0000000000";
-   constant RETURN_OP               : std_logic_vector(0 to 9)  := "0000000001";
-   constant RESET_ADDR_VEC          : std_logic_vector(0 to 9)  := "1000000000";
-   constant FETCH_ADDR_VEC          : std_logic_vector(0 to 9)  := "1000000100";
+   constant RET_OP                  : std_logic_vector(0 to 9)  := "0000000001";
+   constant BRA_RESET_VEC           : std_logic_vector(0 to 9)  := "1000000000";
+   constant BRA_FETCH_VEC           : std_logic_vector(0 to 9)  := "1000000100";
    constant UNUSED                  : std_logic_vector(0 to 25) := "00000000000000000000000000";
 
    -- Microcode ROM 512x36 (18Kbits block RAM)
@@ -67,42 +67,43 @@ architecture rtl of decode is
    -- reset op:
       UNUSED & NEXT_OP,
       UNUSED & NEXT_OP,
-      UNUSED & FETCH_ADDR_VEC,
-      UNUSED & RESET_ADDR_VEC,
+      UNUSED & BRA_FETCH_VEC,
+      UNUSED & BRA_RESET_VEC,
 
    -- init op:
-   
+
    -- fetch op:
       UNUSED & NEXT_OP,
-      UNUSED & RETURN_OP,
-   
+      UNUSED & RET_OP,
+
    -- jump to reset:
-      UNUSED & RESET_ADDR_VEC,
+      UNUSED & BRA_RESET_VEC,
       others => (others => '0')
    );
 
    -- Microcode control
-   signal ctl_mcode_addr_type_s        : std_logic;
-   signal ctl_mcode_addr_next_s        : std_logic_vector(0 to 8);
-   signal mcode_r, mcode_x             : std_logic_vector(0 to 8);
-   signal mcode_stack_r, mcode_stack_x : std_logic_vector(0 to 8);
+   signal ctl_mcode_addr_type_s        : std_logic;                  -- Type of microcode address
+   signal ctl_mcode_addr_next_s        : std_logic_vector(0 to 8);   -- Next microcode address
+   signal mcode_r, mcode_x             : std_logic_vector(0 to 8);   -- Microcode address
+   signal mcode_stack_r, mcode_stack_x : std_logic_vector(0 to 8);   -- One-address stack
    signal mcode_out_r                  : std_logic_vector(0 to 35) := x"000000000";
 
-   
+
    -- Memory cycle FSM
    type memory_t is (MEM_SELECT, MEM_CYCLE);
-   signal memcycle_r, memcycle_x : memory_t;
-   signal cycle_cnt_r, cycle_cnt_x  : std_logic_vector(0 to 4);
+   signal memcycle_r, memcycle_x       : memory_t;                   -- Memory cycle state
+   signal cycle_cnt_r, cycle_cnt_x     : std_logic_vector(0 to 4);   -- Memory cycle counter
 
    -- Memory control signals, registered
-   signal memen_n_r, memen_n_x      : std_logic;
-   signal dbin_r, dbin_x            : std_logic;
-   signal we_n_r, we_n_x            : std_logic;
-   
+   signal memen_n_r, memen_n_x         : std_logic;
+   signal dbin_r, dbin_x               : std_logic;
+   signal we_n_r, we_n_x               : std_logic;
 
+
+   -- TODO temporary signals, replace and delete.
    signal fake_op_r, fake_op_x : std_logic;
    signal start_memcycle_s : std_logic;
-   
+
 begin
 
    -- Opcode Formats
@@ -125,7 +126,7 @@ begin
    -- instructions and modifies the remaining bits to specify the src or
    -- dst of the operation, since the stack always works with R15.
 
-   
+
 --   ctl_wp_sel_s_o <=
 
    -- The byte operator only exists in 8 instructions and is always bit 3.
@@ -158,21 +159,22 @@ begin
    process (clk_i)
    begin if rising_edge(clk_i) then
       if reset_n_i = '0' then
-      
+
          mcode_r        <= (others => '0');
          mcode_stack_r  <= (others => '0');
-         
+
       else
-         
+
+         -- Synchronous BRAM microcode read.  Accessed every cycle.
          mcode_out_r    <= microcode(to_integer(unsigned(mcode_x)));
          mcode_r        <= mcode_x;
          mcode_stack_r  <= mcode_stack_x;
 
       end if;
    end if;
-   end process;   
+   end process;
 
-   
+
    ctl_mcode_addr_type_s     <= mcode_out_r(26);
    ctl_mcode_addr_next_s     <= mcode_out_r(27 to 35);
 
@@ -181,36 +183,40 @@ begin
    MicrocodeCycle :
    process (mcode_r, mcode_stack_r, ctl_mcode_addr_type_s, ctl_mcode_addr_next_s)
    begin
-   
+
+      mcode_x <= mcode_r;
       mcode_stack_x <= mcode_stack_r;
-      
+
       -- Priority encoder to determine next microcode state.
-      
-      
-      -- Test for microcode absolute address jump.
+
+
+      -- Test for microcode absolute address branch.
       if ctl_mcode_addr_type_s = MCODE_JMP then
          mcode_x <= ctl_mcode_addr_next_s;
+         -- Store the return address as the next microcode instruction after
+         -- the branch instruction.
          mcode_stack_x <= mcode_r + 1;
 
       -- Test for microcode return op.
       elsif ctl_mcode_addr_next_s(8) = MCODE_RET then
+         -- The return address is stored on the stack.
          mcode_x <= mcode_stack_r;
 
-      -- Microcode next address.
+      -- Default, next microcode instruction.
       else
          mcode_x <= mcode_r + 1;
 
       end if;
    end process;
 
-   
+
    -- Memory cycle FSM register transfer
    --
    MemoryCycleRT :
    process (clk_i)
    begin if rising_edge(clk_i) then
       if reset_n_i = '0' then
-      
+
          memcycle_r  <= MEM_SELECT;
          cycle_cnt_r <= MEMCYCLE_COUNT;
          memen_n_r   <= '1';
@@ -218,7 +224,7 @@ begin
          we_n_r      <= '1';
 
          fake_op_r   <= '0';
-         
+
       else
 
          memcycle_r  <= memcycle_x;
@@ -226,40 +232,40 @@ begin
          memen_n_r   <= memen_n_x;
          dbin_r      <= dbin_x;
          we_n_r      <= we_n_x;
-         
+
          fake_op_r   <= fake_op_x;
 
       end if;
    end if;
-   end process;   
+   end process;
 
    start_memcycle_s <= '1';
-   
+
    -- Memory cycle next state logic
    --
    MemoryCycle :
    process (memcycle_r, cycle_cnt_r, memen_n_r, dbin_r, we_n_r, fake_op_r, start_memcycle_s)
    begin
-   
+
       memcycle_x  <= memcycle_r;
       cycle_cnt_x <= cycle_cnt_r - 1;
       memen_n_x   <= memen_n_r;
       dbin_x      <= dbin_r;
       we_n_x      <= we_n_r;
-      
+
       fake_op_x   <= fake_op_r;
 
       case memcycle_r is
-      
+
       when MEM_SELECT =>
-      
+
          cycle_cnt_x <= MEMCYCLE_COUNT;
 
          if start_memcycle_s = '1' then
-            
+
             memcycle_x <= MEM_CYCLE;
             memen_n_x <= '0';
-            
+
             if fake_op_r = '0' then
                dbin_x <= '0';
                we_n_x <= '0';
@@ -270,18 +276,18 @@ begin
          end if;
 
       when MEM_CYCLE =>
-      
+
          if cycle_cnt_r = 0 then
-         
+
             memcycle_x <= MEM_SELECT;
             memen_n_x   <= '1';
             dbin_x      <= '0';
             we_n_x      <= '1';
-         
-         
+
+
             fake_op_x <= not fake_op_r;
          end if;
-         
+
       end case;
 
    end process;
